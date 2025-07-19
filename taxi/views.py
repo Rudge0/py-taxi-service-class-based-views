3b513b968839f25740594d646fc035e6c785be2d
Rudge0 <maksim.pariychuk07@gmail.com>
2025-07-20 01:44:58 +0200

    added changes

--- taxi/views.py
+++ taxi/views.py
@@ -17,25 +17,34 @@ def index(request):
 
 class ManufacturerListView(generic.ListView):
     model = Manufacturer
-    queryset = Manufacturer.objects.select_related("car")
+    queryset = Manufacturer.objects.prefetch_related("cars")
     paginate_by = 5
+    template_name = "taxi/manufacturer_list.html"
+    context_object_name = "manufacturers"
 
 
 class CarListView(generic.ListView):
     model = Car
     paginate_by = 5
     queryset = Car.objects.select_related("manufacturer")
+    template_name = "taxi/car_list.html"
+    context_object_name = "cars"
 
 
 class CarDetailView(generic.DetailView):
     model = Car
+    template_name = "taxi/car_detail.html"
 
 
 class DriverDetailView(generic.DetailView):
     model = Driver
     queryset = Driver.objects.all()
+    template_name = "taxi/driver_detail.html"
 
 
 class DriverListView(generic.ListView):
     model = Driver
     paginate_by = 5
+    queryset = Driver.objects.prefetch_related("cars")
+    template_name = "taxi/driver_list.html"
+    context_object_name = "drivers"
